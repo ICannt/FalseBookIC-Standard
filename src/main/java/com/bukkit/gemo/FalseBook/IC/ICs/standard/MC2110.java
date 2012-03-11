c972fac9ffc1383b057a21fe92cd51121d47636a
Harry <haza55@tpg.com.au>
2012-03-11 21:52:10 +1000

    Initial

---
+++ src/main/java/com/bukkit/gemo/FalseBook/IC/ICs/standard/MC2110.java
@@ -0,0 +1,101 @@
+package com.bukkit.gemo.FalseBook.IC.ICs.standard;
+
+import com.bukkit.gemo.FalseBook.IC.FalseBookICCore;
+import com.bukkit.gemo.FalseBook.IC.ICs.BaseChip;
+import com.bukkit.gemo.FalseBook.IC.ICs.BaseIC;
+import com.bukkit.gemo.FalseBook.IC.ICs.ICGroup;
+import com.bukkit.gemo.FalseBook.IC.ICs.InputState;
+import com.bukkit.gemo.FalseBook.IC.ICs.Lever;
+import com.bukkit.gemo.utils.ICUtils;
+import com.bukkit.gemo.utils.Parser;
+import com.bukkit.gemo.utils.SignUtils;
+import java.util.HashMap;
+import java.util.Iterator;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class MC2110 extends BaseIC
+{
+  public HashMap<String, SchedulerClass> TaskList = new HashMap<String, SchedulerClass>();
+
+  public MC2110() {
+    this.ICName = "LOW DELAYER";
+    this.ICNumber = "[MC2110]";
+    setICGroup(ICGroup.STANDARD);
+    this.chipState = new BaseChip(true, true, false, "Clock", "Reset", "");
+    this.chipState.setOutputs("Output", "", "");
+    this.chipState.setLines("delay in seconds", "");
+    this.ICDescription = "The MC2110 delays a low signal for [x] seconds (Line 3), if the signal is stable.";
+  }
+
+  public void checkCreation(SignChangeEvent event)
+  {
+    if (!Parser.isInteger(event.getLine(2))) {
+      SignUtils.cancelSignCreation(event, "Enter the delay in seconds in line 3.");
+      return;
+    }
+    event.setLine(2, String.valueOf(Math.abs(Integer.valueOf(event.getLine(2)).intValue())));
+    event.setLine(3, "");
+  }
+
+  public void Execute(Sign signBlock, InputState currentInputs, InputState previousInputs)
+  {
+    boolean input1High = currentInputs.isInputOneHigh();
+    boolean input2High = currentInputs.isInputTwoHigh();
+    Object newSched;
+    if ((!input1High) && (!input2High))
+    {
+      for (SchedulerClass sched : this.TaskList.values()) {
+        if (sched.equalsLoc(signBlock.getBlock().getLocation())) {
+          Bukkit.getServer().getScheduler().cancelTask(sched.TaskID);
+          this.TaskList.remove(signBlock.getBlock().getLocation().toString());
+          break;
+        }
+      }
+
+      if (!Parser.isInteger(signBlock.getLine(2))) {
+        return;
+      }
+      int secs = Math.abs(Parser.getInteger(signBlock.getLine(2), 1));
+      newSched = new SchedulerClass(this, signBlock.getBlock().getLocation());
+      ((SchedulerClass)newSched).TaskID = Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(FalseBookICCore.getInstance(), (Runnable)newSched, 20 * secs);
+      this.TaskList.put(signBlock.getBlock().getLocation().toString(), (SchedulerClass) newSched);
+    } else if ((input1High) || (input2High))
+    {
+      for (newSched = this.TaskList.values().iterator(); ((Iterator)newSched).hasNext(); ) { SchedulerClass sched = (SchedulerClass)((Iterator)newSched).next();
+        if (sched.equalsLoc(signBlock.getBlock().getLocation())) {
+          Bukkit.getServer().getScheduler().cancelTask(sched.TaskID);
+          this.TaskList.remove(signBlock.getBlock().getLocation().toString());
+          break;
+        }
+      }
+      switchLever(Lever.BACK, signBlock, true);
+    }
+  }
+
+  public class SchedulerClass
+    implements Runnable
+  {
+    public int TaskID = -1;
+    public MC2110 father;
+    public Location signLoc;
+
+    public SchedulerClass(MC2110 father, Location signLoc)
+    {
+      this.father = father;
+      this.signLoc = signLoc;
+    }
+
+    public void run() {
+      ICUtils.switchLever((Sign)this.signLoc.getBlock().getState(), false);
+      this.father.TaskList.remove(this.signLoc.getBlock().getLocation().toString());
+      Bukkit.getServer().getScheduler().cancelTask(this.TaskID);
+    }
+
+    public boolean equalsLoc(Location loc) {
+      return this.signLoc.equals(loc);
+    }
+  }
+}
---
+++ src/main/java/com/bukkit/gemo/FalseBook/IC/ICs/standard/StandardPackage.java
@@ -0,0 +1,49 @@
+package com.bukkit.gemo.FalseBook.IC.ICs.standard;
+
+import com.bukkit.gemo.FalseBook.IC.ICs.ExternalICPackage;
+
+public class StandardPackage extends ExternalICPackage
+{
+  public StandardPackage()
+  {
+    setAPI_VERSION("1.1");
+
+    setShowImportMessages(false);
+    addIC(MC1000.class);
+    addIC(MC1001.class);
+    addIC(MC1017.class);
+    addIC(MC1018.class);
+    addIC(MC1020.class);
+    addIC(MC1025.class);
+    addIC(MC1030.class);
+    addIC(MC1050.class);
+    addIC(MC2020.class);
+    addIC(MC2100.class);
+    addIC(MC2101.class);
+    addIC(MC2110.class);
+    addIC(MC2111.class);
+    addIC(MC2500.class);
+    addIC(MC2501.class);
+    addIC(MC2510.class);
+    addIC(MC2511.class);
+    addIC(MC3000.class);
+    addIC(MC3001.class);
+    addIC(MC3002.class);
+    addIC(MC3003.class);
+    addIC(MC3020.class);
+    addIC(MC3021.class);
+    addIC(MC3030.class);
+    addIC(MC3031.class);
+    addIC(MC3032.class);
+    addIC(MC3033.class);
+    addIC(MC3034.class);
+    addIC(MC3036.class);
+    addIC(MC3040.class);
+    addIC(MC3101.class);
+    addIC(MC4000.class);
+    addIC(MC4010.class);
+    addIC(MC4100.class);
+    addIC(MC4110.class);
+    addIC(MC4200.class);
+  }
+}
